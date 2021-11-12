5e9b99b1b8d8ba9140a2cf57c4a3aeafdfd1ddc4
Adriano Regis <adbrum@outlook.com>
2021-11-12 22:51:50 +0000

    Created models Event and Session

--- events/models.py
+++ events/models.py
@@ -1,3 +1,29 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    session_id = models.ForeignKey(
+        'events.Session',
+        verbose_name='session',
+        related_name='events',
+        on_delete=models.CASCADE
+    )
+    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=150)
+    data = models.JSONField()
+    timestamp = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        ordering = ('-created_at',)
+        verbose_name = 'event'
+        verbose_name_plural = 'events'
+
+
+class Session(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
